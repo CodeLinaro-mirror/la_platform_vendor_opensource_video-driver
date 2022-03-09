6fe8e90101fe12a823125fb1e4a0d78ca4db2f23
Agnimitra Sen <quic_agnimitr@quicinc.com>
2022-03-09 13:24:40 +0530

    msm: vidc: Enable FW unload only for Deepsleep
    Enable FW unload only in Deepsleep for monaco.
    Earlier in the RBSC path FW unload happens.
    The change will restrict FW unload only in case of Deepsleep.
    Change-Id: I6e522ed5f22f5053ec85fd361b3738e603f9043a
    Signed-off-by: Agnimitra Sen <quic_agnimitr@quicinc.com>

--- msm/vidc/msm_vidc_platform.c
+++ msm/vidc/msm_vidc_platform.c
@@ -2199,7 +2199,7 @@ static struct msm_vidc_common_data scuba_common_data[] = {
 static struct msm_vidc_common_data monaco_common_data[] = {
 	{
 		.key = "qcom,never-unload-fw",
-		.value = 0,
+		.value = 1,
 	},
 	{
 		.key = "qcom,fw-unload-delay",
