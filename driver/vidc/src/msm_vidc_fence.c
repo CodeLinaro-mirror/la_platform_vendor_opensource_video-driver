28ff1802cbb7920781c85e7f186edcdadc136395
Akshata Sahukar <quic_asahukar@quicinc.com>
2022-07-07 17:36:20 -0700

    video: driver: Avoid device crash after fence usecase execution
    Since spin lock for fence is per session based and stored in instance
    memory, by the time fence consumer released the fence, driver close
    might be called and instance memory might be cleared. So, when fence
    consumer released the fence, at the time of fence release callback,
    this spin lock might nt be available. This race condition is resulting
    into device crash after fence playback session ends. Avoid this by
    using per fence based spin lock, which decouples it from instance
    memory.
    
    Change-Id: I12d6976c0bac3f0211bb16e24009603b37471ef3
    Signed-off-by: Akshata Sahukar <quic_asahukar@quicinc.com>

--- driver/vidc/src/msm_vidc_fence.c
+++ driver/vidc/src/msm_vidc_fence.c
@@ -38,7 +38,7 @@ static void msm_vidc_dma_fence_release(struct dma_fence *df)
 	if (df) {
 		fence = container_of(df, struct msm_vidc_fence, dma_fence);
 		d_vpr_l("%s: name %s\n", __func__, fence->name);
-		kfree(fence);
+		msm_vidc_vmem_free((void **)&fence);
 	} else {
 		d_vpr_e("%s: invalid fence\n", __func__);
 	}
@@ -52,23 +52,22 @@ static const struct dma_fence_ops msm_vidc_dma_fence_ops = {
 
 struct msm_vidc_fence *msm_vidc_fence_create(struct msm_vidc_inst *inst)
 {
-	struct msm_vidc_fence *fence;
+	struct msm_vidc_fence *fence = NULL;
+	int rc = 0;
 
 	if (!inst) {
 		d_vpr_e("%s: invalid params\n", __func__);
 		return NULL;
 	}
 
-	fence = kzalloc(sizeof(*fence), GFP_KERNEL);
-	if (!fence) {
-		i_vpr_e(inst, "%s: failed to allocate memory for fence\n",
-			__func__);
+	rc = msm_vidc_vmem_alloc(sizeof(*fence), (void **)&fence, __func__);
+	if (rc)
 		return NULL;
-	}
 
 	fence->fd = INVALID_FD;
+	spin_lock_init(&fence->lock);
 	dma_fence_init(&fence->dma_fence, &msm_vidc_dma_fence_ops,
-		&inst->fence_context.lock, inst->fence_context.ctx_num,
+		&fence->lock, inst->fence_context.ctx_num,
 		++inst->fence_context.seq_num);
 	snprintf(fence->name, sizeof(fence->name), "%s: %llu",
 		inst->fence_context.name, inst->fence_context.seq_num);
@@ -219,7 +218,6 @@ void msm_vidc_fence_deinit(struct msm_vidc_inst *inst)
 	}
 	i_vpr_h(inst, "%s: %s\n", __func__, inst->fence_context.name);
 	inst->fence_context.ctx_num = 0;
-	spin_lock_init(&inst->fence_context.lock);
 	snprintf(inst->fence_context.name, sizeof(inst->fence_context.name),
 		"%s", "");
 }
