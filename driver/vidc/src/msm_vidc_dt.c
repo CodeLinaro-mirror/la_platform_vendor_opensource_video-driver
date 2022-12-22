10c04755c8c5aea6c015e021ac5eed54e5c50b37
Sachu George <quic_sachgeor@quicinc.com>
2022-12-21 16:19:14 -0800

    video: driver: Use iommu.h instead of dma-iommu.h
    dma-iommu.h header is deprecated from 6.1 kernel. So remove
    its usage in latest kernel(from 6.1) compilation.
    
    Change-Id: If8e6e192e3320c36484e8348f3cb4215618867fb
    Signed-off-by: Sachu George <quic_sachgeor@quicinc.com>

--- driver/vidc/src/msm_vidc_dt.c
+++ driver/vidc/src/msm_vidc_dt.c
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/iommu.h>
+#include <linux/version.h>
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 16, 0))
 #include <linux/dma-iommu.h>
+#endif
 #include <linux/of.h>
 #include <linux/sort.h>
 
