de83c4b72bce1ca5f4d78b6b9a03d8522351f487
quic_jianxiao <quic_jianxiao@quicinc.com>
2024-09-10 18:21:45 +0800

    video: driver: add support for hibernation
    Add freeze and restore functions in pm ops to support hibernation.
    The video firmware should be unloaded in case of hibernation.
    
    Change-Id: Icc2b8de7e1391e76f93c73df963723ad7b04e07e

--- driver/vidc/src/msm_vidc_probe.c
+++ driver/vidc/src/msm_vidc_probe.c
@@ -599,8 +599,50 @@ static int msm_vidc_pm_resume(struct device *dev)
 	return 0;
 }
 
+#ifdef CONFIG_HIBERNATION
+static int msm_vidc_pm_freeze(struct device *dev)
+{
+	int rc = 0;
+	struct msm_vidc_core *core;
+
+	/*
+	 * Bail out if
+	 * - driver possibly not probed yet
+	 * - not the main device. We don't support power management on
+	 *   subdevices (e.g. context banks)
+	 */
+	if (!dev || !dev->driver ||
+		!of_device_is_compatible(dev->of_node, "qcom,msm-vidc"))
+		return 0;
+
+	core = dev_get_drvdata(dev);
+	if (!core) {
+		d_vpr_e("%s: invalid core\n", __func__);
+		return -EINVAL;
+	}
+
+	d_vpr_h("%s\n", __func__);
+	msm_vidc_core_deinit(core, true);
+
+	if (core->state == MSM_VIDC_CORE_DEINIT) {
+		d_vpr_e("%s: video core uninitialized\n", __func__);
+	}
+
+	return rc;
+}
+
+static int msm_vidc_pm_restore(struct device* dev) {
+	d_vpr_h("%s\n", __func__);
+	return 0;
+}
+#endif
+
 static const struct dev_pm_ops msm_vidc_pm_ops = {
 	SET_SYSTEM_SLEEP_PM_OPS(msm_vidc_pm_suspend, msm_vidc_pm_resume)
+#ifdef CONFIG_HIBERNATION
+	.freeze = msm_vidc_pm_freeze,
+	.restore = msm_vidc_pm_restore,
+#endif
 };
 
 struct platform_driver msm_vidc_driver = {
