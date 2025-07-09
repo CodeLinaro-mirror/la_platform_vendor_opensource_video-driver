lolipuru/kernel_xiaomi_sm8750-modules

video: driver: fix close cmd timeout issue
In stability testing, OPEN & CLOSE cmds were called frequently
so sometimes newly opened inst is not part of instances array
and response_handler is still processing previous session
packets and close response also might come on same iteration.
Since that inst is not available in local instances cache
reverse thread treats that inst as invalid and skips close
response handling and i.e resulting into CLOSE cmd timeout
issue.

Added change to search in &core->instances list, incase inst
is not found in local instances cache.

Change-Id: I229f70e0da3220278fa3a38a3c537ff0d4462335
Signed-off-by: Govindaraj Rajagopal <quic_grajagop@quicinc.com>

driver/vidc/inc/msm_vidc_driver.h
@@ -534,6 +534,8 @@ struct msm_vidc_buffer *get_meta_buffer(struct msm_vidc_inst *inst,
 struct msm_vidc_inst *get_inst_ref_locked(struct msm_vidc_inst *inst);
 struct msm_vidc_inst *get_inst_ref(struct msm_vidc_core *core,
 				   struct msm_vidc_inst *instance);
+struct msm_vidc_inst *get_inst(struct msm_vidc_core *core,
+		u32 session_id);
 void put_inst(struct msm_vidc_inst *inst);
 bool msm_vidc_allow_metadata_delivery(struct msm_vidc_inst *inst,
 				      u32 cap_id, u32 port);

driver/vidc/src/msm_vidc_driver.c
@@ -4721,6 +4721,24 @@ struct msm_vidc_inst *get_inst_ref(struct msm_vidc_core *core,
 	return inst;
 }
 
+struct msm_vidc_inst *get_inst(struct msm_vidc_core *core,
+		u32 session_id)
+{
+	struct msm_vidc_inst *inst = NULL;
+	bool matches = false;
+
+	mutex_lock(&core->lock);
+	list_for_each_entry(inst, &core->instances, list) {
+		if (inst->session_id == session_id) {
+			matches = true;
+			break;
+		}
+	}
+	inst = matches ? get_inst_ref_locked(inst) : NULL;
+	mutex_unlock(&core->lock);
+	return inst;
+}
+
 void put_inst(struct msm_vidc_inst *inst)
 {
 	kref_put(&inst->kref, msm_vidc_close_helper);

driver/vidc/src/venus_hfi.c
@@ -617,7 +617,7 @@ void __unload_fw(struct msm_vidc_core *core)
 	d_vpr_h("%s unloaded video firmware\n", __func__);
 }
 
-static inline struct msm_vidc_inst *get_inst(
+static inline struct msm_vidc_inst *get_inst_cached(
 	struct msm_vidc_inst *const *const instances, const s32 count, u32 session_id)
 {
 	struct msm_vidc_inst *inst = NULL;
@@ -657,15 +657,27 @@ static int __process_msg_q(struct msm_vidc_core *core,
 		if (!hdr->session_id) {
 			rc = handle_system_response(core, hdr);
 		} else {
-			inst = get_inst(instances, num_instances, hdr->session_id);
+			bool local_inst = false;
+
+			inst = get_inst_cached(instances, num_instances, hdr->session_id);
 			if (!inst) {
-				d_vpr_e("%s: Invalid inst - %#x\n", __func__, hdr->session_id);
-				rc = -EINVAL;
-				goto error;
+				d_vpr_l("%s: inst not found in cache - %#x\n",
+					__func__, hdr->session_id);
+				inst = get_inst(core, hdr->session_id);
+				if (!inst) {
+					d_vpr_e("%s: Invalid inst - %#x\n",
+						__func__, hdr->session_id);
+					rc = -EINVAL;
+					goto error;
+				}
+				local_inst = true;
 			}
 			inst_lock(inst, __func__);
 			rc = handle_session_response(inst, hdr);
 			inst_unlock(inst, __func__);
+
+			if (local_inst)
+				put_inst(inst);
 		}
 error:
 		if (rc)

CodeLinaro-mirror/la_platform_vendor_opensource_video-driver

Signed-off-by: Renjiang Han <quic_renjiang@quicinc.com>
(cherry picked from commit c95727d262bf0de67d9bd576d6fcd32705ad263b)

driver/vidc/inc/msm_vidc_driver.h
@@ -611,6 +611,8 @@ struct msm_vidc_buffer *get_meta_buffer(struct msm_vidc_inst *inst,
 struct msm_vidc_inst *get_inst_ref_locked(struct msm_vidc_inst *inst);
 struct msm_vidc_inst *get_inst_ref(struct msm_vidc_core *core,
 				   struct msm_vidc_inst *instance);
+struct msm_vidc_inst *get_inst_using_session_id(struct msm_vidc_core *core,
+		u32 session_id);
 void put_inst(struct msm_vidc_inst *inst);
 bool msm_vidc_allow_metadata_delivery(struct msm_vidc_inst *inst,
 				      u32 cap_id, u32 port);

driver/vidc/src/msm_vidc_driver.c
@@ -5324,6 +5324,24 @@ struct msm_vidc_inst *get_inst_ref(struct msm_vidc_core *core,
 	return inst;
 }
 
+struct msm_vidc_inst *get_inst_using_session_id(struct msm_vidc_core *core,
+		u32 session_id)
+{
+	struct msm_vidc_inst *inst = NULL;
+	bool matches = false;
+
+	mutex_lock(&core->lock);
+	list_for_each_entry(inst, &core->instances, list) {
+		if (inst->session_id == session_id) {
+			matches = true;
+			break;
+		}
+	}
+	inst = matches ? get_inst_ref_locked(inst) : NULL;
+	mutex_unlock(&core->lock);
+	return inst;
+}
+
 void put_inst(struct msm_vidc_inst *inst)
 {
 	kref_put(&inst->kref, msm_vidc_close_helper);

driver/vidc/src/venus_hfi.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/soc/qcom/llcc-qcom.h>
@@ -685,7 +685,7 @@ void __unload_fw(struct msm_vidc_core *core)
 	d_vpr_h("%s unloaded video firmware\n", __func__);
 }
 
-static inline struct msm_vidc_inst *get_inst(
+static inline struct msm_vidc_inst *find_catched_instance(
 	struct msm_vidc_inst *const *const instances, const s32 count, u32 session_id)
 {
 	struct msm_vidc_inst *inst = NULL;
@@ -725,15 +725,27 @@ static int __process_msg_q(struct msm_vidc_core *core,
 		if (!hdr->session_id) {
 			rc = handle_system_response(core, hdr);
 		} else {
-			inst = get_inst(instances, num_instances, hdr->session_id);
+			bool local_inst = false;
+
+			inst = find_catched_instance(instances, num_instances, hdr->session_id);
 			if (!inst) {
-				d_vpr_e("%s: Invalid inst - %#x\n", __func__, hdr->session_id);
-				rc = -EINVAL;
-				goto error;
+				d_vpr_l("%s: inst not found in cache - %#x\n",
+					__func__, hdr->session_id);
+				inst = get_inst_using_session_id(core, hdr->session_id);
+				if (!inst) {
+					d_vpr_e("%s: Invalid inst - %#x\n",
+						__func__, hdr->session_id);
+					rc = -EINVAL;
+					goto error;
+				}
+				local_inst = true;
 			}
 			inst_lock(inst, __func__);
 			rc = handle_session_response(inst, hdr);
 			inst_unlock(inst, __func__);
+
+			if (local_inst)
+				put_inst(inst);
 		}
 error:
 		if (rc)