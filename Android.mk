5e059096d9de911654a762f48d7355f506d07142
Robert Qiu <rqiu@qti.qualcomm.com>
2025-07-21 09:54:04 -0400

    video-driver: Enable compiling with bazel for HQX
    1. Fix compiling error as the legacy compilation (non DDK)
       is still used by default. Use DDK flag.
       gen5_gvm_gy seems no need of it but no harm.
    
    2. Fix syntax error when running bazel command lines
       by removing tabs from BUILD.bazel.
    
    Change-Id: I3edbabd6929011c4c635f6c0faa6e6c923e41aa0
    Signed-off-by: Robert Qiu <rqiu@qti.qualcomm.com>

--- Android.mk
+++ Android.mk
@@ -53,8 +53,8 @@ LOCAL_MODULE              := msm_video.ko
 LOCAL_MODULE_KBUILD_NAME  := msm_video.ko
 LOCAL_MODULE_TAGS         := optional
 LOCAL_MODULE_DEBUG_ENABLE := true
+LOCAL_MODULE_DDK_BUILD    := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
-
 
 # Include kp_module.ko in the /vendor/lib/modules (vendor.img)
 include $(DLKM_DIR)/Build_external_kernelmodule.mk
