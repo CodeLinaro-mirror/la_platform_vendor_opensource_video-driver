ee2a1b62c13a7f03931d30276101be6c0598655d
Suchand Ghosh <suchghos@qti.qualcomm.com>
2025-07-27 23:11:52 -0700

    Revert "Video: Driver: Add support for yupik"
    Revert submission 6043868-Kodiak-IOT-Video-upgrade-video-kernel.lnx.5.1.r1-rel
    
    Reverted changes: /q/submissionid:6043868-Kodiak-IOT-Video-upgrade-video-kernel.lnx.5.1.r1-rel
    
    Change-Id: I57cd07281672c9518bef1591afa9c53968dc25b1

--- config/yupik_video.h
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: BSD-3-Clause
-/*
- * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
- */
-
-#define CONFIG_MSM_VIDC_YUPIK 1
-#define CONFIG_MSM_VIDC_IRIS2 1
