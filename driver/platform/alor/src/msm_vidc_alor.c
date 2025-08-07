7fd8bc882a25847f10fde5e39e36f3c0929d4987
Rohit Jadhav <rbjadhav@qti.qualcomm.com>
2025-08-07 05:30:35 -0700

    Revert "video-driver: Add change to maintain similarity between alor and canoe caps"
    This reverts commit c27351a0bd022e706ae2d14388355226f01e2fe4.
    
    Reason for revert: R1 revert 
    
    Change-Id: I8a6531606570ce83fbd66c692fc7fcd8a99c7f32
    Signed-off-by: Rohit Jadhav <quic_rbjadhav@quicinc.com>

--- driver/platform/alor/src/msm_vidc_alor.c
+++ driver/platform/alor/src/msm_vidc_alor.c
@@ -2575,12 +2575,7 @@ static struct msm_platform_inst_cap_dependency instance_cap_dependency_data_alor
 		NULL,
 		msm_vidc_set_u32_enum},
 
-	{PROFILE, ENC, HEIC,
-		{META_SEI_MASTERING_DISP, META_SEI_CLL, META_HDR10PLUS},
-		msm_vidc_adjust_profile,
-		msm_vidc_set_u32_enum},
-
-	{PROFILE, ENC, HEVC,
+	{PROFILE, ENC, HEVC | HEIC,
 		{META_SEI_MASTERING_DISP, META_SEI_CLL, META_HDR10PLUS,
 		META_HIST_INFO, META_HDR10_MAX_RGB_INFO},
 		msm_vidc_adjust_profile,
