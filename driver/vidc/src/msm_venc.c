287df39a96153c7be823453fbb8d9c0c137c4c14
Rajathi S <rajathis@qti.qualcomm.com>
2026-04-09 16:56:11 +0530

    video: driver: [DNM]Update Crop offset as input property in AR50LT
       - Crop_offset is output property for ar50lt.
       - For IRIS crop_offset is input and output property
         for the CTS testcase to pass
    
    Change-Id: Ife594a2bc0cf799ebfb988554c8782357750b748

--- driver/vidc/src/msm_venc.c
+++ driver/vidc/src/msm_venc.c
@@ -221,10 +221,11 @@ static int msm_venc_set_raw_resolution(struct msm_vidc_inst *inst,
 		return -EINVAL;
 	}
 
-	resolution = (inst->fmts[port].fmt.pix_mp.width << 16) |
-		inst->fmts[port].fmt.pix_mp.height;
+	resolution = inst->crop.width << 16 | inst->crop.height;
+
 	i_vpr_h(inst, "%s: width: %d height: %d\n", __func__,
-			inst->fmts[port].fmt.pix_mp.width, inst->fmts[port].fmt.pix_mp.height);
+			inst->crop.width, inst->crop.height);
+
 	rc = venus_hfi_session_property(inst,
 			HFI_PROP_RAW_RESOLUTION,
 			HFI_HOST_FLAGS_NONE,
