14183815adbc810ae5796e365d1c6b568d219bc3
Vasantha Balla <c_vballa@qti.qualcomm.com>
2026-06-16 10:31:01 +0530

    video-driver: Allow non UBWC format for interlace output
    AR50LT target does not support UBWC and interlace output is
    in NV12 format, So restrict mandatory UBWC output format check
    only for platform which suports UBWC.
    
    Change-Id: Ifde4ebad4fb1d5e31ad80f159f8d5a7a0ca76525
    Signed-off-by: Vasantha Balla <c_vballa@qti.qualcomm.com>
    (cherry picked from commit be2ae53185a2fef8751c9907e9851e979c802dcf)

--- driver/vidc/inc/msm_vidc_driver.h
+++ driver/vidc/inc/msm_vidc_driver.h
@@ -482,7 +482,7 @@ static inline bool is_multi_view_session(struct msm_vidc_inst *inst)
 		inst->capabilities[PROFILE].value ==
 		V4L2_MPEG_VIDEO_HEVC_PROFILE_MAIN_10_MULTIVIEW));
 }
-
+bool is_ubwc_supported_platform(struct msm_vidc_inst *inst);
 const char *cap_name(enum msm_vidc_inst_capability_type cap_id);
 const char *v4l2_pixelfmt_name(struct msm_vidc_inst *inst, u32 pixelfmt);
 const char *v4l2_type_name(u32 port);
